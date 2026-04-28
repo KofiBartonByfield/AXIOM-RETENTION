d82c48b0d44c1398edfaf25abade3d37ffceda1e
Kofi Barton-Byfield <kbartonbyfield@gmail.com>
2026-04-28 17:01:42 +0100

    public host

--- 3. Website/app.py
+++ 3. Website/app.py
@@ -287,4 +287,4 @@ def inject_user():
 
 if __name__ == "__main__":
     # host="0.0.0.0" tells Flask to listen on all public IPs
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
